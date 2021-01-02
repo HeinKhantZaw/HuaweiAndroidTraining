24b37b37715d4662ecfd86805c3c66f7b62a7145
Hein Khant Zaw <heinkhantzaw1@gmail.com>
2021-01-02 18:27:44 +0630

    add day2 folder

--- Day1/FragmentsExample/app/src/main/java/com/example/fragmentsexample/FragmentA.java
+++ Day1/FragmentsExample/app/src/main/java/com/example/fragmentsexample/FragmentA.java
@@ -60,8 +60,8 @@ public class FragmentA extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-            printLog("onCreate() method called");
         }
+        printLog("onCreate() method called");
     }
 
     @Override
