da4cb367bf84fe14590dc9f48b0533ef1748187d
Hein Khant Zaw <heinkhantzaw1@gmail.com>
2021-01-05 18:25:50 +0630

    add normal toggle button in material design example

---
+++ Day2/LayoutExamples/app/src/main/java/com/example/layoutexamples/LinearLayoutHorizonalActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutexamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLayoutHorizonalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout_horizonal);
+    }
+}
